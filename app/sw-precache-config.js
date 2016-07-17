1ef429997a05b43a4682ad24b5ca0e42283f7d46
Witus13 <myfrom.13th@gmail.com>
2016-07-17 16:36:42 +0000

    Bug fixes & new features

--- app/sw-precache-config.js
+++ app/sw-precache-config.js
@@ -4,8 +4,6 @@ module.exports = {
     '/index.html',
     '/manifest.json',
     '/bower_components/webcomponentsjs/webcomponents-lite.min.js',
-    'https://www.gstatic.com/firebasejs/3.0.2/firebase.js', // Those two can be unnescesry
-    'https://apis.google.com/js/platform.js' // Those two can be unnescesry
   ],
   navigateFallback: '/index.html'
 };
