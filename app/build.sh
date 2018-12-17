f33a92eaae82c4a8fc678e77e27be0a882ec0487
Wiktor Olejniczak <myfrom.13th@gmail.com>
2018-12-17 18:50:48 +0100

    Moving to NPM and simplifying project structure

--- app/build.sh
+++
@@ -1,9 +0,0 @@
-#!bin/bash
-
-polymer build;
-cd build;
-cp -R minified final/;
-rm -r final/bower_components/firebase/*;
-cp -r full-js/bower_components/firebase/* final/bower_components/firebase;
-rm -r final/src/yta-onboarding.html;
-cp full-css/src/yta-onboarding.html final/src;
